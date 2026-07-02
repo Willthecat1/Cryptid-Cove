d4f7ab61d6a434e01e172c598497f150b1a472c5
Willthecat1 <Bobbertrand123@gmail.com>
2026-07-01 23:17:44 -0400

    1.0.7

---
+++ 1.0.7/Content/Maps/Vedning.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Vedning" tilewidth="32" tileheight="64" tilecount="1" columns="1">
+ <image source="../Graphics/ItemSprites/Vend.png" width="32" height="64"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="6.375" y="43.25" width="19.75" height="20.125"/>
+  </objectgroup>
+ </tile>
+</tileset>
